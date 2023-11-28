f005c420d910540f6e90b8b38a98fa99b4fe37cf
Mykola Kuievda <dankraig23@gmail.com>
2023-11-28 12:04:48 +0000

    Add favicon, add cursor focus and make key Enter a game runner

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,28 @@ document.addEventListener("DOMContentLoaded", function () {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        });
+        })
     }
+    
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
 
     runGame("addition");
 
-});
+})
 
 /**
  * The Main Game loop, called when the script is first loaded
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
